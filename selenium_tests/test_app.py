5f5a9577800cf91a2a20fd8dd2fc2d62540a41d1
eshangulati <gulatieshan@outlook.com>
2024-10-15 16:29:55 +1100

    Update test_app.py

--- selenium_tests/test_app.py
+++ selenium_tests/test_app.py
@@ -1,19 +1,26 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 import unittest
 
 class MyAppTests(unittest.TestCase):
+
     def setUp(self):
+        # Set up remote connection to Selenium container
+        chrome_options = webdriver.ChromeOptions()
         self.driver = webdriver.Remote(
-            command_executor='http://selenium:4444/wd/hub',  # Connect to the Selenium container
-            desired_capabilities={'browserName': 'chrome'}
+            command_executor='http://selenium:4444/wd/hub',
+            options=chrome_options
         )
-
+    
     def test_home_page(self):
-        driver = self.driver
-        driver.get("http://app:80")  # 'app' refers to the application container
-        self.assertIn("Welcome to my website", driver.page_source)
-
+        # Visit the application URL
+        self.driver.get('http://app:80')  # Assuming the app is running on port 80 in the container
+        # Verify the title of the homepage
+        self.assertIn("Welcome", self.driver.title)
+    
     def tearDown(self):
+        # Close the browser after the test
         self.driver.quit()
 
 if __name__ == "__main__":
